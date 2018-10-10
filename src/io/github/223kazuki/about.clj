ef2e055dfc1462b0b87c20ef2d9fa05eca6ae8f3
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2018-10-09 23:07:35 -0700

    Update.

--- src/io/github/223kazuki/about.clj
+++ src/io/github/223kazuki/about.clj
@@ -6,34 +6,37 @@
    config
    [:div.about.fit
     [:p.h1 "About"]
-    [:p "Kazuki Tsutsumi / 堤 一樹"]
+    [:ul
+      [:li "Kazuki Tsutsumi / 堤 一樹"]
+      [:li "Clojure Engineer"]]
     [:p.h2 "Career"]
     [:table.col-12
      [:tr
-      [:td.col-3 "Apr/2006 - Mar/2010"]
-      [:td "Nagoya University (Astronomy)"]]
+      [:td "May/2017 - Now"]
+      [:td "Senior System Engineer @ San Jose, CA"]]
+     [:tr
+      [:td "Apr/2012 - May/2017"]
+      [:td "System Engineer @ Tokyo"]]
+     [:tr
+      [:td "Apr/2012 - Now"]
+      [:td "General Manager @ Kokugakuin University Sumo Club / 國學院大學相撲部監督"]]
      [:tr
       [:td "Apr/2010 - Mar/2012"]
       [:td "Tokyo Institute of Technology (Astrophysics)"]]
      [:tr
-      [:td "Apr/2012 - May/2017"]
-      [:td "System Engineer @ Japanese System Integrator"]]
-     [:tr
-      [:td "May/2017 - Now"]
-      [:td "System Engineer @ Research Center in San Jose, CA"]]
-     [:tr
-      [:td "Apr/2012 - Now"]
-      [:td "General Manager @ Kokugakuin University Sumo Club / 國學院大學相撲部監督"]]]
+      [:td.col-3 "Apr/2006 - Mar/2010"]
+      [:td "Nagoya University (Astronomy)"]]]
     [:p.h2 "SNS Accounts"]
     [:ul
      [:li "Twitter: " [:a {:href "//twitter.com/goronao"} "@goronao"]]
      [:li "GitHub: " [:a {:href "//github.com/223kazuki"} "223kazuki"]]
      [:li "Slide Share: " [:a {:href "//www.slideshare.net/ssuser8b0ea4"} "Kazuki Tsutsumi"]]]
-    [:p.h2 "Interests"]
+    [:p.h2 "Skills/Interests"]
     [:ul
+     [:li "Clojure / ClojureScript"]
      [:li "Web Development"]
-     [:li "clojure / clojurescript"]
-     [:li "java"]
-     [:li "javascript"]
-     [:li "Android Development"]
-     [:li "Rust"]]]))
+     [:li "Java"]
+     [:li "JavaScript"]
+     [:li "React.js"]
+     [:li "Rust"]
+     [:li "Blockchain"]]]))
--- src/io/github/223kazuki/index.clj
+++ src/io/github/223kazuki/index.clj
@@ -8,12 +8,13 @@
 (defn render [{posts :entries :as config}]
   (layout
    config
-   [:div.index.clearfix.mt3
-    (for [{:keys [title permalink tags date-published content] :as post} posts]
-      [:div
-       [:p.h2
-        [:a {:href (:permalink post)} (:title post)]]
-       [:div.fit.clearfix
-        [:time.left (f/unparse formatter (c/from-date date-published))]
-        (for [tag tags]
-          [:a.ml1.tag.left {:href (str "/tags/" tag ".html")} tag])]])]))
+   [:div.index
+     [:p.h1 "Posts"]
+     (for [{:keys [title permalink tags date-published content] :as post} posts]
+       [:div
+        [:p.h2
+         [:a {:href (:permalink post)} (:title post)]]
+        [:div.fit.clearfix
+         [:time.left (f/unparse formatter (c/from-date date-published))]
+         (for [tag tags]
+           [:a.ml1.tag.left {:href (str "/tags/" tag ".html")} tag])]])]))
